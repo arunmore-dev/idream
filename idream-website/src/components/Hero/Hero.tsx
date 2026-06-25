95191995380f91ee670403e813b805a80c54dc33
Arun More <arun.more@vselindia.com>
2026-06-25 12:23:06 +0530

    fix: simplify arrow SVG remove clipPath

--- idream-website/src/components/Hero/Hero.tsx
+++ idream-website/src/components/Hero/Hero.tsx
@@ -35,16 +35,15 @@ export default function Hero() {
             Explore Our Vision
           </Link>
           <Link href="#investors" className={styles.btnOutline}>
-            Investor Relation
-            <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="29"
-  height="29"
-  viewBox="0 0 29 29"
-  fill="none"
-  className={styles.arrowIcon}
->
-  <g clipPath="url(#clip0_41_565)">
+  Investor Relation
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="29"
+    height="29"
+    viewBox="0 0 29 29"
+    fill="none"
+    className={styles.arrowIcon}
+  >
     <path
       d="M7.90792 6.77812H21.4642V20.3343M20.5227 7.71952L6.77823 21.464"
       stroke="white"
@@ -52,14 +51,8 @@ export default function Hero() {
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-  </g>
-  <defs>
-    <clipPath id="clip0_41_565">
-      <rect width="28.2421" height="28.2421" rx="14.1211" fill="white" />
-    </clipPath>
-  </defs>
-</svg>
-          </Link>
+  </svg>
+</Link>
         </div>
       </div>
     </section>
